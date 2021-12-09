9bbf280ce48e8642d260a9d2dde9c305f99d48c5
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2021-12-09 22:37:51 +0600

    58 Length of Last Word)

--- 58.LengthofLastWord.js
+++ 58.LengthofLastWord.js
@@ -5,6 +5,13 @@
  var lengthOfLastWord = function(s) {
     const arr = s.split(' ');
     for (var i = arr.length-1; i>=0; i--) {
-        if (arr[i] !== '')  return arr[i].length;
+        if (arr[i] !== '') return arr[i].length;
     }
 };
+console.log(lengthOfLastWord("Hello   World   "));
+
+
+//Trim solution
+var lengthOfLastWord2 = function(s) {
+return s.trim().split(" ").pop().length;
+}
