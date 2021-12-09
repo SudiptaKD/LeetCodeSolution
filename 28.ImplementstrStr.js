47008159c723ffe246287693c79f84f3700dd8c8
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2021-12-09 21:24:12 +0600

    28 Implement strStr()

---
+++ 28.ImplementstrStr.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    let isThere = -1;
+    if ( needle === '' ) isThere = 0;
+    for( let i = 0; i <= haystack.length-needle.length ; i++ ) {
+        if( haystack[i] == needle[0] ) {
+            for (let j = 0 ; j < needle.length ; j++ ) {
+                if (haystack[i+j] !== needle[j] )  {
+                    break;
+                }
+                else if (j== needle.length-1) {
+                    isThere = i;
+                }
+            }
+            if (isThere !== -1) break;
+        }
+    }
+    return isThere;
+};
+console.log(strStr('hello', 'll'))
