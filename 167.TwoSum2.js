4c155f781f6c13782427455247eece9b524ee92e
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-01-18 19:24:21 +0600

    167.TwoSum2.js

--- 167.TwoSum2.js
+++ 167.TwoSum2.js
@@ -22,3 +22,16 @@
     }
     return returnArr;
 };
+
+//Optimised solution
+
+var twoSum = function(numbers, target) {
+    let low = 0
+    let high = numbers.length - 1
+    while (low < high) {
+        let sum = numbers[low] + numbers[high]
+        if (sum === target) return [low+1, high+1]
+        else if (sum > target) high--
+        else low++
+    }
+}
