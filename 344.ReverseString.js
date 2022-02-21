a5c51410020f3470e7436a3bc1138200aef80b58
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-02-22 04:20:36 +0600

    344 recursion added

--- 344.ReverseString.js
+++ 344.ReverseString.js
@@ -9,3 +9,20 @@
       s[j]= x;
   }  
 };
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    function reversing(s,i) {
+        if (i >= s.length/2) {
+            return;
+        }
+        let a = s[i];
+        s[i] = s[s.length-1-i];
+        s[s.length-1-i] = a;
+        reversing(s, i+1);
+    }
+    reversing(s, 0);
+   };
